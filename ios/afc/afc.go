4352ba9c335d59762a3fb48b829b8e89a283c6c3
colerwang <colerwang@tencent.com>
2022-07-07 11:49:59 +0800

    add: afc exception handle

--- ios/afc/afc.go
+++ ios/afc/afc.go
@@ -2,6 +2,7 @@ package afc
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 )
@@ -30,6 +31,98 @@ const (
 	Afc_Mode_APPEND   uint64 = 0x00000005
 	Afc_Mode_RDAPPEND uint64 = 0x00000006
 )
+
+const (
+	Afc_Err_Success                = 0
+	Afc_Err_UnknownError           = 1
+	Afc_Err_OperationHeaderInvalid = 2
+	Afc_Err_NoResources            = 3
+	Afc_Err_ReadError              = 4
+	Afc_Err_WriteError             = 5
+	Afc_Err_UnknownPacketType      = 6
+	Afc_Err_InvalidArgument        = 7
+	Afc_Err_ObjectNotFound         = 8
+	Afc_Err_ObjectIsDir            = 9
+	Afc_Err_PermDenied             = 10
+	Afc_Err_ServiceNotConnected    = 11
+	Afc_Err_OperationTimeout       = 12
+	Afc_Err_TooMuchData            = 13
+	Afc_Err_EndOfData              = 14
+	Afc_Err_OperationNotSupported  = 15
+	Afc_Err_ObjectExists           = 16
+	Afc_Err_ObjectBusy             = 17
+	Afc_Err_NoSpaceLeft            = 18
+	Afc_Err_OperationWouldBlock    = 19
+	Afc_Err_IoError                = 20
+	Afc_Err_OperationInterrupted   = 21
+	Afc_Err_OperationInProgress    = 22
+	Afc_Err_InternalError          = 23
+	Afc_Err_MuxError               = 30
+	Afc_Err_NoMemory               = 31
+	Afc_Err_NotEnoughData          = 32
+	Afc_Err_DirNotEmpty            = 33
+)
+
+func getError(errorCode uint64) error {
+	switch errorCode {
+	case Afc_Err_UnknownError:
+		return errors.New("UnknownError")
+	case Afc_Err_OperationHeaderInvalid:
+		return errors.New("OperationHeaderInvalid")
+	case Afc_Err_NoResources:
+		return errors.New("NoResources")
+	case Afc_Err_ReadError:
+		return errors.New("ReadError")
+	case Afc_Err_WriteError:
+		return errors.New("WriteError")
+	case Afc_Err_UnknownPacketType:
+		return errors.New("UnknownPacketType")
+	case Afc_Err_InvalidArgument:
+		return errors.New("InvalidArgument")
+	case Afc_Err_ObjectNotFound:
+		return errors.New("ObjectNotFound")
+	case Afc_Err_ObjectIsDir:
+		return errors.New("ObjectIsDir")
+	case Afc_Err_PermDenied:
+		return errors.New("PermDenied")
+	case Afc_Err_ServiceNotConnected:
+		return errors.New("ServiceNotConnected")
+	case Afc_Err_OperationTimeout:
+		return errors.New("OperationTimeout")
+	case Afc_Err_TooMuchData:
+		return errors.New("TooMuchData")
+	case Afc_Err_EndOfData:
+		return errors.New("EndOfData")
+	case Afc_Err_OperationNotSupported:
+		return errors.New("OperationNotSupported")
+	case Afc_Err_ObjectExists:
+		return errors.New("ObjectExists")
+	case Afc_Err_ObjectBusy:
+		return errors.New("ObjectBusy")
+	case Afc_Err_NoSpaceLeft:
+		return errors.New("NoSpaceLeft")
+	case Afc_Err_OperationWouldBlock:
+		return errors.New("OperationWouldBlock")
+	case Afc_Err_IoError:
+		return errors.New("IoError")
+	case Afc_Err_OperationInterrupted:
+		return errors.New("OperationInterrupted")
+	case Afc_Err_OperationInProgress:
+		return errors.New("OperationInProgress")
+	case Afc_Err_InternalError:
+		return errors.New("InternalError")
+	case Afc_Err_MuxError:
+		return errors.New("MuxError")
+	case Afc_Err_NoMemory:
+		return errors.New("NoMemory")
+	case Afc_Err_NotEnoughData:
+		return errors.New("NotEnoughData")
+	case Afc_Err_DirNotEmpty:
+		return errors.New("DirNotEmpty")
+	default:
+		return nil
+	}
+}
 
 type AfcPacketHeader struct {
 	Magic         uint64
@@ -60,7 +153,6 @@ func Decode(reader io.Reader) (AfcPacket, error) {
 	if err != nil {
 		return AfcPacket{}, err
 	}
-
 	contentPayloadLength := header.Entire_length - header.This_length
 	payload := make([]byte, contentPayloadLength)
 	_, err = io.ReadFull(reader, payload)
