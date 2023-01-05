1abba706636392e87fa4b1b0c78c10bba5f40c27
brianhu <brianhu@tencent.com>
2023-01-05 17:07:18 +0800

    ref:add addr&&udid param

--- cmd/wdb/wdb.go
+++ cmd/wdb/wdb.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/danielpaulus/go-ios/wdbd/ioskit"
-	log "github.com/sirupsen/logrus"
+	"os"
 	"path"
 	"runtime"
+
+	"github.com/danielpaulus/go-ios/ios"
+	"github.com/danielpaulus/go-ios/wdbd/ioskit"
+	log "github.com/sirupsen/logrus"
 )
 
 var usbmuxdPath = flag.String("usbmuxd-path", "unix:/var/run/usbmuxd", "usbmuxd path")
-var port = flag.Int("port", 8083, "wdbd server port")
+var addr = flag.String("addr", "", "remote usbmuxd addr")
+var udid = flag.String("udid", "", "remote device udid")
 
 func initLog() {
 	log.SetFormatter(&log.JSONFormatter{})
@@ -25,13 +29,24 @@ func initLog() {
 	log.SetLevel(log.InfoLevel)
 }
 
+func initUsbmuxd() {
+	// 处理已存在的usbmuxd socket
+	if runtime.GOOS != "windows" {
+		if fileInfo, _ := os.Stat(ios.DefaultUsbmuxdSocket); fileInfo != nil {
+			bak := fmt.Sprintf("%v.bak", ios.DefaultUsbmuxdSocket)
+			_ = os.Rename(ios.DefaultUsbmuxdSocket, bak)
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 	initLog()
-
-	remoteDevice := ioskit.NewRemoteDevice(
-		"192.168.0.47:8083",
-		"82d8ccbcd9160681f7fd9d377d8e0dff7c6591a5")
+	initUsbmuxd()
+	if *addr == "" || *udid == "" {
+		log.Panicln("addr param and udid param are required")
+	}
+	remoteDevice := ioskit.NewRemoteDevice(*addr, *udid)
 	log.Infof("connected to remote device: %+v", remoteDevice)
 	go func() {
 		log.Panicln(remoteDevice.Monitor(context.Background()))
